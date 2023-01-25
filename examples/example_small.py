6a98da93e9abdc7ab61c078840f34e13d3dc712f
edovanveen <edo.vanveen@vortech.nl>
2023-01-25 15:40:26 +0100

    Fixed random seed, example output now the same as reference output

--- examples/example_small.py
+++ examples/example_small.py
@@ -3,6 +3,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+import random
 import numpy as np
 import pandas as pd
 from src.ARTEMIS import run_artemis
@@ -11,14 +12,15 @@ from src.ARTEMIS import run_artemis
 scenario_file = 'example_config.csv'  # Config file that needs to be run
 output_subfolder = '../../../examples/example_output/' # determines that the output should be written to a subfolder in the regular output folder (if not define output_subfolder = '')
 
-# Run the simulation.
+# Run the simulation
+random.seed(0)  # Make sure we always get the same result.
 np.random.seed(0)  # Make sure we always get the same result.
 run_artemis(scenario_file, output_subfolder)
 
 # Make sure the output is the same as it was before.
 df1_example = pd.read_csv('example_output/flat_time_x_agent_resultsdefault.csv')
 df1_ref = pd.read_csv('reference_output/flat_time_x_agent_resultsdefault.csv')
-print(df1_example.compare(df1_ref))
+assert(df1_example.compare(df1_ref).empty)
 df2_example = pd.read_csv('example_output/flat_time_x_environment_resultsdefault.csv')
 df2_ref = pd.read_csv('reference_output/flat_time_x_environment_resultsdefault.csv')
-print(df2_example.compare(df2_ref))
+assert(df2_example.compare(df2_ref).empty)
